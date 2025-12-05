fa979e95238504d2633ecf323fc9f438396edf5b
Bryan <bryanfinesw@gmail.com>
2025-12-05 12:24:42 -0500

    Parents page finished

--- src/modules/parents.js
+++ src/modules/parents.js
@@ -1,20 +1,144 @@
 import { Header } from "./header";
 import { Footer } from "./footer";
 import { EventHandler } from "./displayController";
+import { prayers } from "./prayers";
 
 export const Parents = (() => {
 
-   const el = {
-      content: () => document.getElementById('content'),
-   }
+    const el = {
+        content: () => document.getElementById('content'),
+    }
 
-   const init = () => {
-      el.content().innerHTML = "";
+    const init = () => {
+        el.content().innerHTML = "";
 
-      Header.init();
-      Footer.init();
-      EventHandler.bindEvents();
-   }
+        Header.init();
+        el.content().appendChild(createPage());
+        Footer.init();
+        EventHandler.bindEvents();
+    }
 
-   return { init }
+    const createPage = () => {
+        const section = document.createElement("section");
+        const parentContainer = document.createElement("div");
+
+        section.className = "parent-section";
+        parentContainer.className = "parent-container";
+
+        parentContainer.append(parentsBlurb(), tinyDisciples(), biblicalVirtues());
+        section.appendChild(parentContainer);
+        return section;
+    }
+
+    const parentsBlurb = () => {
+        const div = document.createElement("div");
+        const parentHeading = document.createElement("h1");
+        const parentInformation = document.createElement("p");
+
+        parentHeading.textContent = "Parents!";
+        parentInformation.textContent = `Hi! My name is Julie and I have a love for God and families. A little bit about me - I’m
+                    married to an amazing guy who puts up with my craziness. We have 4 grown kids, and
+                    each of them have made the decision to follow Christ. There is nothing that makes me
+                    happier than knowing they each have their own relationship with Jesus. That’s what it’s
+                    all about, isn’t it? For us as parents to raise our kids in the faith and knowledge of Jesus
+                    Christ.
+                    I graduated from Valley Forge Christian College with a B.S. in Biblical Studies. I have
+                    worked in kid’s ministry for many years. I was the fun, creative story teller on Sunday
+                    mornings. Now, I work at an elementary school as a paraprofessional. My goal as an
+                    author is to write books with a Christian theme enabling families to have conversations
+                    about Jesus. It’s not always easy to start a conversation with your kids, believe me I
+                    know. One thing that I like to do is include the Scripture from which that story is drawn.
+                    Read the book. Read the Bible. Ask questions to your kids. Let them ask you questions.
+                    Thanks for taking the time to read up about me and inviting me into your family! The
+                    battle for our kids starts at home, so suit up, grab those Swords, and be alert because
+                    the enemy is on the prowl looking for someone to devour(1 Peter 5:8).`;
+
+        div.append(parentHeading, parentInformation);
+        return div;
+    }
+
+    const tinyDisciples = () => {
+        const div = document.createElement("div");
+        const discipleHeader = document.createElement("h1");
+        const list = document.createElement("ul");
+
+        discipleHeader.textContent = "Raising Tiny Disciples";
+        list.innerHTML = `<li>Put them in the kid’s service at your church. Start them young! It’s never
+                        too soon
+                        to experience God’s love. Even babies can hear songs about Jesus and His
+                        love.</li>
+                    <li>Get together with other families who have kids and share the same values you
+                        have. Our kids were blessed to be surrounded by friends who were a
+                        little older
+                        than them. They had great examples of what it looked like for someone
+                        close to
+                        their age to follow Jesus, and our friends were an example of what Godly
+                        parenting looked like.</li>
+                    <li>Read parenting books written by Christian authors or authors who seem to
+                        have
+                        the same values as you. Some of my favorites are:
+                        <ul id="last-ul">
+                            <li>Kevin Leman, Making Children Mind Without Losing Yours</li>
+                            <li>Elisa Morgan, Real Moms</li>
+                            <li>Lisa Bevere, Girls With Swords</li>
+                            <li>Patricia H. Rushford, What Kids Need Most in a Mom</li>
+                            <li>Gary Chapman, The 5 Love Languages</li>
+                        </ul>
+                    </li>
+                    <li>Pray. Pray. Then, pray some more. I have included an article I have had
+                        since
+                        before I even had my own children. 31 Biblical Virtues to Pray for Your
+                        Kids was
+                        my guide to praying Scripture over my kids. Now, I use this to pray over
+                        my
+                        granddaughter and future grandchildren.</li>`;
+
+        div.append(discipleHeader, list);
+        return div;
+    }
+
+    const biblicalVirtues = () => {
+        const container = document.createElement("div");
+        const header = document.createElement("h1");
+        const ul = document.createElement("ul");
+
+        header.textContent = "31 Biblical Virtues to Pray for Your Kids";
+        container.className = "prayer-list";
+        container.appendChild(header);
+
+        prayers.forEach(prayerObj => {
+            const li = document.createElement("li");
+
+            const title = document.createElement("b");
+            title.textContent = prayerObj.title;
+
+            const p = document.createElement("p");
+            p.textContent = prayerObj.prayer;
+
+            const span = document.createElement("span");
+            prayerObj.references.forEach((ref, i) => {
+                const a = document.createElement("a");
+                a.href = ref.url;
+                a.target = "_blank";
+                a.textContent = ref.text;
+                span.appendChild(a);
+
+                if (i < prayerObj.references.length - 1) {
+                    span.appendChild(document.createTextNode("; "));
+                }
+            });
+
+            li.appendChild(title);
+            li.appendChild(document.createTextNode(" - "));
+            li.appendChild(p);
+            li.appendChild(span);
+
+            ul.appendChild(li);
+        });
+        container.appendChild(ul);
+        return container;
+    }
+
+
+    return { init }
 })()
---
+++ src/modules/prayers.js
@@ -0,0 +1,224 @@
+export const prayers = [
+    {
+        title: "Salvation",
+        prayer: "Lord, let salvation spring up within my children, that they may obtain the salvation that is in Christ Jesus, with eternal glory.",
+        references: [
+            { text: "Isa. 45:8", url: "https://biblia.com/bible/esv/isaiah/45/8" },
+            { text: "2 Tim. 2.10", url: "https://biblia.com/bible/esv/2-timothy/2/10" }
+        ]
+    },
+    {
+        title: "Growth in Grace",
+        prayer: "I pray that my children may grow in the grace and knowledge of our Lord and Savior Jesus Christ.",
+        references: [
+            { text: "2 Pet. 3:18", url: "https://biblia.com/bible/esv/2-peter/3/18" }
+        ]
+    },
+    {
+        title: "Love",
+        prayer: "Grant, Lord, that my children may learn to live a life of love, through the Spirit, who dwells in them.",
+        references: [
+            { text: "Gal. 5:25", url: "https://biblia.com/bible/esv/galatians/5/25" },
+            { text: "Eph. 5:2", url: "https://biblia.com/bible/esv/ephesians/5/2" }
+        ]
+    },
+    {
+        title: "Honesty and Integrity",
+        prayer: "May integrity and honesty be their virtue and their protection.",
+        references: [
+            { text: "Ps. 25:21", url: "https://biblia.com/bible/esv/psalm/25/21" }
+        ]
+    },
+    {
+        title: "Self-Control",
+        prayer: "Father, help my children not to be like many others around them, but let them be alert and self-controlled in all they do.",
+        references: [
+            { text: "1 Thess. 5:6", url: "https://biblia.com/bible/esv/1-thessalonians/5/6" }
+        ]
+    },
+    {
+        title: "Love for God's Word",
+        prayer: "May my children grow to find Your Word more precious than much pure gold and sweeter than honey from the comb.",
+        references: [
+            { text: "Ps. 19:10", url: "https://biblia.com/bible/esv/psalm/19/10" }
+        ]
+    },
+    {
+        title: "Justice",
+        prayer: "God, help my children to love justice as You do and act justly in all they do.",
+        references: [
+            { text: "Ps. 11:7", url: "https://biblia.com/bible/esv/psalm/11/7" },
+            { text: "Mic. 6:8", url: "https://biblia.com/bible/esv/micah/6/8" }
+        ]
+    },
+    {
+        title: "Mercy",
+        prayer: "May my children always be merciful, just as their Father is merciful.",
+        references: [
+            { text: "Luke 6:36", url: "https://biblia.com/bible/esv/luke/6/36" }
+        ]
+    },
+    {
+        title: "Respect (for self, others, and authority)",
+        prayer: "Father, grant that my children may show proper respect to everyone, as Your Word commands.",
+        references: [
+            { text: "1 Pet. 2:17", url: "https://biblia.com/bible/esv/1-peter/2/17" }
+        ]
+    },
+    {
+        title: "Biblical Self-Esteem",
+        prayer: "Help my children develop a strong self-esteem that is rooted in the realization that they are God's workmanship, created in Christ jesus.",
+        references: [
+            { text: "Eph. 2:10", url: "https://biblia.com/bible/esv/ephesians/2/10" }
+        ]
+    },
+    {
+        title: "Faithfulness",
+        prayer: "Let love and faithfulness never leave my children, but bind these twin virtues around their necks and write them on the tablet of their hearts.",
+        references: [
+            { text: "Prov. 3:3", url: "https://biblia.com/bible/esv/proverbs/3/3" }
+        ]
+    },
+    {
+        title: "Courage",
+        prayer: "May my children always be strong and courageous in their character.",
+        references: [
+            { text: "Deut. 31:6", url: "https://biblia.com/bible/esv/deuteronomy/31/6" }
+        ]
+    },
+    {
+        title: "Purity",
+        prayer: "Create in them a pure heart, O God, and let that purity of heart be shown in their actions.",
+        references: [
+            { text: "Ps. 51:10", url: "https://biblia.com/bible/esv/psalm/51/10" }
+        ]
+    },
+    {
+        title: "Kindness",
+        prayer: "Lord, may my children always try to be kind to each other and to everyone else.",
+        references: [
+            { text: "1 Thess. 5:15", url: "https://biblia.com/bible/esv/1-thessalonians/5/15" }
+        ]
+    },
+    {
+        title: "Generosity",
+        prayer: "Grant that my children may be generous and willing to share, and so lay up treasure for themselves as a firm foundation for the coming age.",
+        references: [
+            { text: "1 Tim. 6:18-19", url: "https://biblia.com/bible/esv/1-timothy/6/18-19" }
+        ]
+    },
+    {
+        title: "Peace-Loving",
+        prayer: "Father, let my children make every effort to do what leads to peace.",
+        references: [
+            { text: "Rom. 14:19", url: "https://biblia.com/bible/esv/romans/14/19" }
+        ]
+    },
+    {
+        title: "Joy",
+        prayer: "May my children be filled with the joy given by the Holy Spirit.",
+        references: [
+            { text: "1 Thess. 1:6", url: "https://biblia.com/bible/esv/1-thessalonians/1/6" }
+        ]
+    },
+    {
+        title: "Perseverance",
+        prayer: "Lord, teach my children perseverance in all they do, and help them especially to run with perseverance the race marked out for them.",
+        references: [
+            { text: "Heb. 12:1", url: "https://biblia.com/bible/esv/hebrews/12/1" }
+        ]
+    },
+    {
+        title: "Humility",
+        prayer: "God, please cultivate in my children the ability to show true humility toward all.",
+        references: [
+            { text: "Titus 3:2", url: "https://biblia.com/bible/esv/titus/3/2" }
+        ]
+    },
+    {
+        title: "Compassion",
+        prayer: "Lord, please clothe my children with the virtue of compassion.",
+        references: [
+            { text: "Col. 3:12", url: "https://biblia.com/bible/esv/colossians/3/12" }
+        ]
+    },
+    {
+        title: "Responsibility",
+        prayer: "Grant that my children may learn responsibility, for each one should carry his own load.",
+        references: [
+            { text: "Gal. 6:5", url: "https://biblia.com/bible/esv/galatians/6/5" }
+        ]
+    },
+    {
+        title: "Contentment",
+        prayer: "Father, teach my children the secret of being content in any and every situation, through Him who gives them strength.",
+        references: [
+            { text: "Phil. 4:12-13", url: "https://biblia.com/bible/esv/philippians/4/12-13" }
+        ]
+    },
+    {
+        title: "Faith",
+        prayer: "I pray that faith will find root and grow in my children's hearts, that by faith they may gain what has been promised to them.",
+        references: [
+            { text: "Luke 17:5-6", url: "https://biblia.com/bible/esv/luke/17/5-6" },
+            { text: "Heb. 11:1-40", url: "https://biblia.com/bible/esv/hebrews/11/1-40" }
+        ]
+    },
+    {
+        title: "A Servant's Heart",
+        prayer: "God, please help my children develop servant's hearts, that they may serve wholeheartedly, as if they were serving the Lord, not men.",
+        references: [
+            { text: "Eph. 6:7", url: "https://biblia.com/bible/esv/ephesians/6/7" }
+        ]
+    },
+    {
+        title: "Hope",
+        prayer: "May the God of hope grant that my children may overflow with hope and hopefulness by the power of the Holy Spirit.",
+        references: [
+            { text: "Rom. 15:13", url: "https://biblia.com/bible/esv/romans/15/13" }
+        ]
+    },
+    {
+        title: "Willingness and Ability to Work",
+        prayer: "Teach my children, Lord, to value work and to work at it with all their heart, as working for the Lord and not for men.",
+        references: [
+            { text: "Col. 3:23", url: "https://biblia.com/bible/esv/colossians/3/23" }
+        ]
+    },
+    {
+        title: "Passion for God",
+        prayer: "Lord, please instill in my children a soul that 'followeth hard after thee', one that clings passionately to You.",
+        references: [
+            { text: "Ps. 63:8", url: "https://biblia.com/bible/esv/psalm/63/8" }
+        ]
+    },
+    {
+        title: "Self-Discipline",
+        prayer: "Father, I pray that my children may acquire a disciplined and prudent life, doing what is right and just and fair.",
+        references: [
+            { text: "Prov. 1:3", url: "https://biblia.com/bible/esv/proverbs/1/3" }
+        ]
+    },
+    {
+        title: "Prayerfulness",
+        prayer: "Grant, Lord, that my children's lives may be marked by prayerfulness, that they may learn to pray in the Spirit on all occasions with all kinds of prayers.",
+        references: [
+            { text: "1 Thess. 5:17", url: "https://biblia.com/bible/esv/1-thessalonians/5/17" }
+        ]
+    },
+    {
+        title: "Gratitude",
+        prayer: "Help my children to live lives that are always overflowing with thankfulness and always giving thanks to God the Father for everything, in the name of our Lord Jesus Christ.",
+        references: [
+            { text: "Eph. 5:20", url: "https://biblia.com/bible/esv/ephesians/5/20" },
+            { text: "Col. 2:7", url: "https://biblia.com/bible/esv/colossians/2/7" }
+        ]
+    },
+    {
+        title: "A Heart for Missions",
+        prayer: "Lord, please help my children to develop a desire to see Your glory declared among the nations, Your marvelous deeds among the peoples.",
+        references: [
+            { text: "Psalm 96:3", url: "https://biblia.com/bible/esv/Ps96.3" }
+        ]
+    }
+];
