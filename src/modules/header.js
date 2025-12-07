b01f47446bf32df04116ea737887c204f04af1e9
Bryan <bryanfinesw@gmail.com>
2025-12-06 19:44:21 -0500

    snow effect

--- src/modules/header.js
+++ src/modules/header.js
@@ -7,7 +7,70 @@ export const Header = (() => {
     }
 
     const render = () => {
+        el.content().appendChild(snowCanvas());
+        snowEffect();
         createHeader();
+    }
+
+    const snowCanvas = () => {
+        const snow = document.createElement("canvas");
+        snow.id = "snow-canvas";
+        return snow;
+    }
+
+    const snowEffect = () => {
+        const canvas = document.getElementById("snow-canvas");
+        const ctx = canvas.getContext("2d");
+
+        let flakes = [];
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        function createFlakes() {
+            flakes.push({
+                x: Math.random() * canvas.width,
+                y: Math.random() * canvas.height,
+                r: Math.random() * 3 + 1,
+                d: Math.random() + 1
+            });
+        }
+
+        function drawFlakes() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            ctx.fillStyle = "white";
+            ctx.beginPath();
+            flakes.forEach(flake => {
+                ctx.moveTo(flake.x, flake.y);
+                ctx.arc(flake.x, flake.y, flake.r, 0, Math.PI * 2);
+            });
+            ctx.fill();
+
+            updateFlakes();
+        }
+
+        function updateFlakes() {
+            flakes.forEach(flake => {
+                flake.y += flake.d;
+                flake.x += Math.sin(flake.y * 0.01);
+
+                if (flake.y > canvas.height) {
+                    flake.y = -5;
+                    flake.x = Math.random() * canvas.width;
+                }
+            });
+        }
+
+        setInterval(() => {
+            if (flakes.length < 100) createFlakes();
+            drawFlakes();
+        }, 30);
+
+        window.onresize = () => {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        };
+
     }
 
     const createHeader = () => {
